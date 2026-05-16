238b2e8abd630f252494b12d13ae4f60bd3e06bd
Kajal Chahal <chahalkajal11@gmail.com>
2026-05-16 14:39:25 +0530

    added

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ dotenv .config()
 
 app.use("/api", authRoutes)
 const port = process.env.PORT
-
+app.get("/", (req, res) => {
+  res.send("Auth API is running ");
+});
 app.listen(port ,()=>{
     console.log(`Server is run on URL:http://localhost:${port}`)
     dbConnect()
