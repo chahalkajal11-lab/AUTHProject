2183602c406d85a9e99a086a6ce2e67671e5de69
Kajal Chahal <chahalkajal11@gmail.com>
2026-05-17 15:00:17 +0530

    completed

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,18 +3,10 @@ import { signUp } from "../Controller/controller.js"
 import {  Login} from "../Controller/UserController.js"
 import Uploader from "../MIddleware/multer.js"
 
-const authRoutes = express.Router()
-
-authRoutes.get("/signup",(req,res)=>{
-   res.send("Signup Route Working 🚀")
-})
+const  authRoutes = express.Router()
 
 authRoutes.post("/signup",Uploader.single("image"),signUp)
-
-authRoutes.get("/login",(req,res)=>{
-   res.send("Login Route Working 🚀")
-})
-
 authRoutes.post("/login", Login)
+
 
 export default authRoutes
