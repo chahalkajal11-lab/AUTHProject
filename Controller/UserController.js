03cf35492175bd590955ca2937b23beaff95804f
Kajal Chahal <chahalkajal11@gmail.com>
2026-05-17 15:20:35 +0530

    mycomplete

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -41,18 +41,5 @@ export const Login = async(req,res)=>{
     }
 }
 
-export const getAllUser = async (req,res)=>{
-  try {
-    let user = await Main.find()
-     res.status(200).json({
-            status:"Success",
-            data:user
-          })
-  } catch (error) {
-      res.status(400).json({
-            status:"Fail",
-            message:`Error   ${error.message}`
-          })
-  }
-}
+
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,10 @@
 import express from "express"
 import { signUp } from "../Controller/controller.js"
-import { getAllUser,Login} from "../Controller/UserController.js"
+import { Login} from "../Controller/UserController.js"
 import Uploader from "../MIddleware/multer.js"
 
 const  authRoutes = express.Router()
 
-authRoutes.post("/signup",Uploader.single("image"),signUp)
-authRoutes.post("/login", Login)
-authRoutes.get("/",getAllUser)
-
+authRoutes.get("/signup",Uploader.single("image"),signUp)
+authRoutes.get("/login", Login)
 export default authRoutes
